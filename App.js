5468fa5b0c8d939c062239b5226947fb6fdec862
Jaewon Kim <jaewon@Jaewons-MBP.cust.communityfibre.co.uk>
2020-10-01 20:52:15 +0100

    first Commit

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, 
+         Text, 
+         View, 
+         StatusBar, 
+         TextInput, 
+         Dimensions,
+         Platform } from 'react-native';
+
+const { height, width } = Dimensions.get("window")
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>Fabio To Do</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New To Do"}/>
+      </View>
     </View>
   );
 }
@@ -14,8 +24,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FDB927',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
+  title: {
+    color:'#fff',
+    fontSize: 30,
+    marginTop: 60,
+    marginBottom: 30,
+    fontWeight: '200'
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex:1,
+    width:width - 25,
+    //borderRadius: 10
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor:"rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+
+      }
+
+    })
+  },
+
 });
